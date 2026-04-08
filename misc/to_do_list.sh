2819a7cfdd8c194edefc84e128c85e4714ee6614
Saffron <qwertysaffron@tutamai.com>
2026-04-09 00:30:57 +0530

    changed the to do from txt to sh

---
+++ misc/to_do_list.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+norm="\033[1;0m"
+red="\033[1;31m"
+green="\033[1;32m"
+yellow="\033[1;33m"
+blue="\033[1;34m"
+
+echo -e "
+$green[✓]Set up virtualization
+$blue[]Exam form
+[]Yt 18 may
+[]Two VMs communicating
+[]Basic Wireshark packet capture$norm"
+# ---
+#
+# Install Windows Server
+#
+# Create domain
+#
+# Join client machine
+#
+# ---
+#
+# Learn how login actually works
+#
+# Explore users, groups, privileges
+#
+# Install BloodHound
+#
+# ---
+#
+# Begin basic AD attack techniques in lab
+#
+# Pass-the-hash conceptually
+#
+# Kerberoasting conceptually
+
+
